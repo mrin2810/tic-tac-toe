e904190d846a744732f871f27eab86d3801a7647
mrinmayee.rane <mrinmayee.rane@zeuslearning.com>
2021-09-23 16:59:36 +0530

    added nbButtonsModule and changed styling for buttons in the square component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,11 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button *ngIf="!value">{{ value }}</button>
-    <button *ngIf="value == 'X'">{{ value }}</button>
-    <button *ngIf="value == 'O'">{{ value }}</button>
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; border: none; outline: none; box-sizing: border-box; }']
 })
 export class SquareComponent {
   @Input()
